5ba5e562ffe851f7d209901198146699d0592abb
Jérôme Foray <moi@foray-jero.me>
2014-08-12 00:28:00 +0200

    Created Games plugin

---
+++ Plugins/Games/Plugin.php
@@ -0,0 +1,44 @@
+<?php namespace Plugins\Games;
+
+use Phergie\Irc\Plugin\React\Command\CommandEvent;
+use Phergie\Irc\Bot\React\EventQueueInterface;
+use Phergie\Irc\Bot\React\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+
+  public function getSubscribedEvents()
+  {
+    return array(
+      'command.roulette'       => 'handleRouletteCommand',
+      'command.roulette.help'  => 'handleRouletteHelp',
+    );
+  }
+
+  public function handleRouletteCommand(CommandEvent $event, EventQueueInterface $queue)
+  {
+    $connection = $event->getConnection();
+    $nick = $event->getNick();
+    $source = $event->getSource();
+
+    // Ignore events sent directly to the bot rather than to a channel
+    if ($connection->getNickname() === $source) {
+      return;
+    }
+
+    if ((rand() % 6) == 0) {
+      $queue->ircPrivmsg($source, "Pan t'es mort !");
+      $queue->ircKick($source, $nick, "Pan t'es mort !");
+    } else {
+      $queue->ircPrivmsg($source, "Tu vis, au suivant !");
+    }
+
+  }
+
+  public function handleRouletteHelp(CommandEvent $event, EventQueueInterface $queue)
+  {
+    $channel = $event->getSource();
+    $message = '.roulette: Pan ! Va tu survivre a la roulette ?';
+    $queue->ircPrivmsg($channel, $message);
+  }
+}
