73af15d44e19b742d6fee622742da565f3ab19f5
Jerome Foray <moi@foray-jero.me>
2015-08-13 15:50:01 +0200

    Updqted to latest phergie, fixed mimeawareurlhandler

--- config.php
+++ config.php
@@ -5,107 +5,98 @@ use Phergie\Irc\Plugin\React\EventFilter\Plugin as EventFilterPlugin;
 use Phergie\Irc\Plugin\React\EventFilter as Filters;
 use WyriHaximus\Phergie\Plugin\Url\Mime;
 
-Dotenv::load(__DIR__);
-Dotenv::required(['IRC_CHANS', 'IRC_HOST', 'IRC_NAME', 'IRC_NICK', 'IRC_IDENT']);
+(new josegonzalez\Dotenv\Loader(__DIR__ . '/.env'))
+    ->parse()
+    ->expect('IRC_CHANS', 'IRC_HOST', 'IRC_NAME', 'IRC_NICK', 'IRC_IDENT')
+    ->putenv();
 
 $client = new \Phergie\Irc\Client\React\Client();
-$client->on('connect.end', function(\Phergie\Irc\ConnectionInterface $connection, \Psr\Log\LoggerInterface $logger) use ($client) {
+$client->on('connect.end', function (\Phergie\Irc\ConnectionInterface $connection, \Psr\Log\LoggerInterface $logger) use ($client) {
     $logger->debug('Connection to ' . $connection->getServerHostname() . ' lost, attempting to reconnect');
     $client->addConnection($connection);
 });
 
-$usermodePlugin = new \Phergie\Irc\Plugin\React\UserMode\Plugin;
+$usermodePlugin = new MakeKlat00\Phergie\Irc\Plugin\ChanModes\Plugin;
 
 $commandPlugins = [
-  new \Plugins\User\Plugin(['userMode' => $usermodePlugin]),
-  new \Plugins\Games\Plugin(),
+    new \Plugins\User\Plugin(['userMode' => $usermodePlugin]),
+    new \Plugins\Games\Plugin(),
 ];
 
 return [
 
-  // Client override
-  'client' => $client,
+    // Client override
+    'client' => $client,
 
-  // Plugins to include for all connections
+    // Plugins to include for all connections
 
-  'plugins' => array_merge([
-    // dependencies
-    new \WyriHaximus\Phergie\Plugin\Dns\Plugin,
-    new \WyriHaximus\Phergie\Plugin\Http\Plugin,
-    new \WyriHaximus\Phergie\Plugin\Url\Plugin([
-      'handler' => new \Plugins\Url\MimeAwareUrlHandler(
-        '%image-mime% %image-width%×%image-height%',
-        [new Mime\Image]
-      ),
-    ]),
-    new \WyriHaximus\Phergie\Plugin\Url\Plugin([
-      'handler' => new \Plugins\Url\MimeAwareUrlHandler(
-        '%title%',
-        [new Mime\Html]
-      ),
-    ]),
-    $usermodePlugin,
+    'plugins' => array_merge([
+        // dependencies
+        new \WyriHaximus\Phergie\Plugin\Dns\Plugin,
+        new \WyriHaximus\Phergie\Plugin\Http\Plugin,
+        new \WyriHaximus\Phergie\Plugin\Url\Plugin([
+            'handler' => new \Plugins\Url\MimeAwareUrlHandler([
+                [
+                    'pattern' => '%title%',
+                    'mimes' => [new Mime\Html]
+                ],
+                [
+                    'pattern' => '%image-mime% %image-width%×%image-height%',
+                    'mimes' => [new Mime\Image]
+                ],
+            ]),
+        ]),
+        $usermodePlugin,
 
-    // runtime essentials
-    new \Phergie\Irc\Plugin\React\Pong\Plugin,
-    new \Phergie\Irc\Plugin\React\AutoJoin\Plugin(['channels' => getenv('IRC_CHANS')]),
-    new \Phergie\Irc\Plugin\React\NickServ\Plugin(['password' => getenv('NICKSERV_PASS') ?: 'foobar']),
+        // runtime essentials
+        new \Phergie\Irc\Plugin\React\Pong\Plugin,
+        new \Phergie\Irc\Plugin\React\AutoJoin\Plugin(['channels' => getenv('IRC_CHANS')]),
+        new \Phergie\Irc\Plugin\React\NickServ\Plugin(['password' => getenv('NICKSERV_PASS') ?: '']),
+        new \Plugins\KickJoin\Plugin,
 
-    new EventFilterPlugin(array(
-      'filter' => new Phergie\Irc\Plugin\React\EventFilter\UserModeFilter($usermodePlugin, array('q', 'a', 'o')),
-      'plugins' => array(
-        new \Phergie\Irc\Plugin\React\JoinPart\Plugin,
-        new \Phergie\Irc\Plugin\React\Quit\Plugin(['message' => 'because %s said so']),
-      ),
-    )),
+        // Restricted
+        new EventFilterPlugin(array(
+            'filter' => new Phergie\Irc\Plugin\React\EventFilter\OrFilter([
+                new Phergie\Irc\Plugin\React\EventFilter\UserFilter(["*!*@foray-jero.me"]),
+                new Plugins\EventFilter\ChanModesFilter($usermodePlugin, array('q', 'a', 'o')),
+            ]),
+            'plugins' => array(
+                new \Phergie\Irc\Plugin\React\JoinPart\Plugin,
+                new \Phergie\Irc\Plugin\React\Quit\Plugin(['message' => 'because %s said so']),
+            ),
+        )),
 
-    // new EventFilterPlugin(array(
-    //   'filter' => new Filters\AndFilter(array(
-    //     new Filters\NotFilter(
-    //       new Filters\UserFilter(array('Hades!*@*'))
-    //     ),
-    //     new Filters\NotFilter(
-    //       new Filters\UserFilter(array('*bot!*@*'))
-    //     ),
-    //   )),
-    //   'plugins' => array(
-    //     new \Plugins\Twitter\Plugin,
-    //   ),
-    // )),
+        // new EventFilterPlugin(array(
+        //   'filter' => new Filters\AndFilter(array(
+        //     new Filters\NotFilter(
+        //       new Filters\UserFilter(array('Hades!*@*'))
+        //     ),
+        //     new Filters\NotFilter(
+        //       new Filters\UserFilter(array('*bot!*@*'))
+        //     ),
+        //   )),
+        //   'plugins' => array(
+        //     new \Plugins\Twitter\Plugin,
+        //   ),
+        // )),
 
-    // commands
-    new \Phergie\Irc\Plugin\React\Command\Plugin(['prefix' => '.']),
-    new \Phergie\Irc\Plugin\React\CommandHelp\Plugin([
-      'plugins'  => $commandPlugins,
-    ]),
-    new \Phergie\Irc\Plugin\React\YouTube\Plugin(array('key' => getenv('GOOGLE_APIKEY') ?: '')),
-    new \Plugins\Twitter\Plugin,
-    new \Plugins\KickJoin\Plugin,
-  ], $commandPlugins),
+        // Url handlers
+        new \Phergie\Irc\Plugin\React\YouTube\Plugin(array('key' => getenv('GOOGLE_APIKEY') ?: '')),
+        new \Plugins\Twitter\Plugin,
 
-  'connections' => [
+        // commands
+        new \Phergie\Irc\Plugin\React\Command\Plugin(['prefix' => '.']),
+        new \Phergie\Irc\Plugin\React\CommandHelp\Plugin([
+            'plugins' => $commandPlugins,
+        ]),
+    ], $commandPlugins),
 
-    new Connection([
-
-      // Required settings
-
-      'serverHostname' => getenv('IRC_HOST'),
-      'username' => getenv('IRC_NAME'),
-      'realname' => getenv('IRC_NICK'),
-      'nickname' => getenv('IRC_IDENT'),
-
-      // Optional settings
-
-      // 'hostname' => 'user server name goes here if needed',
-      // 'serverport' => 6697,
-      // 'password' => 'password goes here if needed',
-      // 'options' => [
-      //   'transport' => 'ssl',
-      //   'force-ipv4' => true,
-      // ]
-
-    ]),
-
-  ]
-
+    'connections' => [
+        new Connection([
+            'serverHostname' => getenv('IRC_HOST'),
+            'username' => getenv('IRC_NAME'),
+            'realname' => getenv('IRC_NICK'),
+            'nickname' => getenv('IRC_IDENT'),
+        ]),
+    ]
 ];
